8e7c9c28e59d9306e9e7253fdfdd4804eaa480c8
Takhir <guvalakat1603@gmail.com>
2026-02-07 17:39:41 +0500

    h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into books(name, author) values ('The Great Gatsby', 'F. Scott Fitzgerald');
+insert into books(name, author) values ('To Kill a Mockingbird', 'Harper Lee');
+insert into books(name, author) values ('1984', 'George Orwell');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table books(
+    id serial primary key,
+    name varchar(255) not null,
+    author varchar(255) not null
+);
