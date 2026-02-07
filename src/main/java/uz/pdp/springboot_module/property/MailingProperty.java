7b771174889ecb55322629a173a8dff526e7d861
Takhir <guvalakat1603@gmail.com>
2026-02-07 16:16:31 +0500

    Springboot module is starting.

---
+++ src/main/java/uz/pdp/springboot_module/property/MailingProperty.java
@@ -0,0 +1,13 @@
+package uz.pdp.springboot_module.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "mailing")
+public record MailingProperty(
+        String host,
+        Integer port,
+        String username,
+        String password,
+        String secretKey
+) {
+}
