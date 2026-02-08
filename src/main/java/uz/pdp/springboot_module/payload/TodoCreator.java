096b0b539d5de380da6a6fe20b311fefa729f43b
Takhir <guvalakat1603@gmail.com>
2026-02-08 12:50:53 +0500

    @Min

--- src/main/java/uz/pdp/springboot_module/payload/TodoCreator.java
+++ src/main/java/uz/pdp/springboot_module/payload/TodoCreator.java
@@ -1,15 +1,13 @@
 package uz.pdp.springboot_module.payload;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record TodoCreator(
         @NotBlank
         @Size(min = 3, max = 100)
         String title,
         @NotNull
+        @Min(1)
         Integer priority
 ) {
 }
