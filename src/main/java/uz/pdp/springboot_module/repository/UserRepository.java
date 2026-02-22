7f5b857c3377d609ff6d23505c24967e81299d4b
Takhir <guvalakat1603@gmail.com>
2026-02-22 13:27:54 +0500

    spring data REST is over

--- src/main/java/uz/pdp/springboot_module/repository/UserRepository.java
+++ src/main/java/uz/pdp/springboot_module/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package uz.pdp.springboot_module.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uz.pdp.springboot_module.entity.User;
 
+@RepositoryRestResource(path = "odamlar", collectionResourceRel = "odamlar", itemResourceRel = "odam")
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 }
