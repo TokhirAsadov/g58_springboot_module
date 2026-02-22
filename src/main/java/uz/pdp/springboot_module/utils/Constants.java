c6279c95593e3e198dd0fed298d1e881d9904bc4
Takhir <guvalakat1603@gmail.com>
2026-02-22 13:15:18 +0500

    spring data REST is starting

--- src/main/java/uz/pdp/springboot_module/utils/Constants.java
+++
@@ -1,9 +0,0 @@
-package uz.pdp.springboot_module.utils;
-
-public interface Constants {
-    String VERSION = "/v1";
-    String DEVICE_MOBILE = "/mobile";
-    String DEVICE_BROWSER = "/browser";
-    String BASE_URL = VERSION+DEVICE_BROWSER+"/api";
-    String BASE_MOBILE_URL = VERSION+DEVICE_MOBILE+"/api";
-}
